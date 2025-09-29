8f072d4df3a6f95863720bacb9b67cd8f42583f6
stephaneG-frep <hoopscmoi@gmail.com>
2025-09-29 10:11:50 +0200

    new session

--- views/question.php
+++ views/question.php
@@ -5,7 +5,7 @@ ini_set("display_errors", 1);
 
 require_once "../models/Users.php";
 require_once "../models/Question.php";
-//require_once "../db/config.php";
+require_once "../db/config.php";
 require_once "../include/head.php";
 require_once "../include/nav_burger.php";
 //require_once "../include/navigation.php";
